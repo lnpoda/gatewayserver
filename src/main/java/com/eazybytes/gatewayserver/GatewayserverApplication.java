45a8299ce5e19045f5582db594a7ccec5f0f05fe
ln <>
2025-04-29 11:58:23 -0700

    created gateway server for eazybank

---
+++ src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,25 @@
+package com.eazybytes.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/eazybank/accounts/**")
+						.filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.build();
+	}
+}
